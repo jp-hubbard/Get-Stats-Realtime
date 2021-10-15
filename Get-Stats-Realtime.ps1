c0e0d729d0487bd67d55014bbabc18a862f81fdc
John Hubbard <john.p.hubbard@intel.com>
2021-10-15 09:21:30 -0700

    Updated help

--- Get-Stats-Realtime.ps1
+++ Get-Stats-Realtime.ps1
@@ -1,11 +1,42 @@
+<#
+ .Synopsis
+  Collect and export vSphere realtime statistitcs over a user defined Period of time.
+
+ .Description
+  Export vSphere realtime statistitcs over a user defined Period of time. This function 
+  builds upon the functionality of the PowerCLI "Get-Stat" Cmdlet. By default, real-time 
+  statistics are collected every 20 seconds for the last 60-minutes. This data is stored
+  in a circular buffer and as new data is collected, the oldest data is overwritten. 
+  This function exports the realtime statistics to a comma seperate file (CSV) every 
+  Period for a specified Duration. Realtime statistics include memory, cpu, disk, and 
+  network.
+
+ .Parameter Entity
+  Fully qualified domain name (FQDN) or IP address of a vSphere host.
+
+ .Parameter Path
+  The file Path where results are exported. File Path assumes a Microsoft Windows client.
+
+ .Parameter Duration
+  The length of time in seconds the function should run.
+
+ .Parameter Period
+  The interval in seconds that statistics are collected and exported. Periods must be
+  between 0-3600 seconds to maintain contigiuous data sets.
+
+ .Example
+   # Collect realtime statistics from the vSphere host "vmhost.example.com" every 45 minutes 
+   for 2 hours and export to the "C:\stats" file path.
+   Get-Stats-Realtime -Entity vmhost.example.com -Path C:\stats -Duration 7200 -Period 2700
+#>
 function Get-Stats-Realtime {
     [CmdletBinding()]
 	param(
-		[string]$entity,
-        [string]$path,
-        [int]$duration,
+		[string]$Entity,
+        [string]$Path,
+        [int]$Duration,
         [ValidateRange(0,3600)]
-        [int]$period
+        [int]$Period
 	)
     
     # Print notifications to screen
@@ -17,28 +48,28 @@ function Get-Stats-Realtime {
 
     # Start the loop and end when elapsed time is greater than the duation
     $elasped=0
-    while ($elapsed -lt ($duration + 1)) {
+    while ($elapsed -lt ($Duration + 1)) {
 
-        # Subtract the period from the current time to identify start range of time to retrieve stats
-        $start = (Get-Date) - [timespan]::fromseconds($period)
+        # Subtract the Period from the current time to identify start range of time to retrieve stats
+        $start = (Get-Date) - [timespan]::fromseconds($Period)
         
         # Get the statistics with the VMware Cmdlet
-        Status "Getting Realtime Stats" "$start $entity"
-        Status "Status" "$elapsed/$duration elapsed/duration in seconds"
+        Status "Getting Realtime Stats" "$start $Entity"
+        Status "Status" "$elapsed/$Duration elapsed/Duration in seconds"
         # Retrieve all statistics
-        $stats = Get-Stat -Entity $entity -Memory -Cpu -Disk -Network -Realtime -Start $start | Select-Object Timestamp,Entity,MetricId,Instance,Value,Unit
+        $stats = Get-Stat -Entity $Entity -Memory -Cpu -Disk -Network -Realtime -Start $start | Select-Object Timestamp,Entity,MetricId,Instance,Value,Unit
 
 	    # Create the name with a timestamp (Windows Pathing)
 	    $time = (Get-Date $stats[0].Timestamp -format u).Replace(':','.').Replace(' ','_')
-	    $name = "$path\realtime-" + $time + '.csv'
+	    $name = "$Path\realtime-" + $time + '.csv'
 
         # Export the statistics to a CSV file 
         Status "Exporting Realtime Stats" "$name"
 	    $stats | Export-CSV -NoTypeInformation $name
 
         # Increment the elapsed time and sleep until next loop
-        Status "Sleeping" "$period seconds"
-        Start-Sleep $period
-        $elapsed = $elapsed + $period      
-    } # End duration loop
+        Status "Sleeping" "$Period seconds"
+        Start-Sleep $Period
+        $elapsed = $elapsed + $Period      
+    } # End Duration loop
 } 
